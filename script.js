0b89ca8384919a4df8936bf986671b78b4ebb37c
rodrigogaleano <rodrigo1galeano@gmail.com>
2022-02-10 22:13:10 -0400

    Ajustes de layout

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function sortear() {
+
     let min = document.getElementById('minimo').value;
     let max = document.getElementById('maximo').value;
+    var res = document.querySelector('#resultado');
+
+    if (min - max >= 0) {
+        alert('Informe um intervalo de números válido');
+        return;
+    }
+
     min = Math.ceil(min);
     max = Math.floor(max);
     let numSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.getElementById('resultado').innerText = 'Resultado: ' + numSorteado;
+    res.innerText = numSorteado;
 }
